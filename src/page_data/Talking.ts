59e0cce11efedd5347f4596804f58bac8f2973a8
MS-POWERCT <46989010+MS-POWERCT@users.noreply.github.com>
2026-08-22 10:19:42 +0800

    1

--- src/page_data/Talking.ts
+++ src/page_data/Talking.ts
@@ -5,104 +5,109 @@ export default {
   // 注意：图片请用 vh-img-flex 类包裹
   data: [
     {
+      'date': '2026-08-20 22:30',
+      'tags': ['日常', '卤牛腱子'],
+      'content': '上次的牛腱子吃的很开心，很成功所以我决定再做一份，这次买的更多2.5斤，同样的做法同样的配料，味道没什么区别。<br/>但是还是增加了一些经验，原来一头牛有12个牛腱子，又分为不同的名字比如：金钱腱，三花腱，肉腱等，不同的腱子肉做不同的食物会好吃很多<br/>所以我下次还要再使用最好的腱子肉做一次'
+    },
+    {
       'date': '2026-08-11 22:30',
-      'tags':['日常', '德州扑克'],
-      'content': '家里电费见底了，我和父亲还有哥哥都在老家，所以决定玩德州扑克，输了都冲电费。上一次我输50，这次输250。<br/>哎，还是不够沉稳，但<b> all in </b>那把我<b> 910JQK </b>,遇到<b> 10JQKA </b>我有什么办法'
+      'tags': ['日常', '德州扑克'],
+      'content': '家里没电费了，父亲还有哥哥都在老家，所以决定玩德州扑克，输了都冲电费。上一次我输50，这次输250。<br/>哎，还是不够沉稳，但<b> all in </b>那把我<b> 910JQK </b>,遇到<b> 10JQKA </b>我有什么办法'
     },
     {
       'date': '2026-08-11 18:30',
-      'tags':['日常', '卤牛腱子'],
+      'tags': ['日常', '卤牛腱子'],
       'content': '今天做了一份卤牛腱子，还挺简单的，买好牛腱子买好桂皮八角一些东西，反正跟着视频中去做，就做出来了味道很不错。<br/> 然后发现以前不做是因为不会，现在要考虑的是牛腱子还挺贵的，一斤多做出来后并没有多少，不过至少干净吧，<br/>计算一下料加牛肉大概72元，做出来的肉用来搭配面应该可以做8-10碗，这样就比较经济了平时自己在家煮面放点青菜然后切点牛腱子就很不错'
     },
     {
       'date': '2026-08-10 22:30',
-      'tags':['日常'],
+      'tags': ['日常'],
       'content': '今天去上饶给麻将馆的自助售货机补货，补了4箱水。然后去万达广场买菜，买了牛腱子。'
     },
     {
-      'date':'2026-08-04 23:30',
-      'tags':['日常'],
+      'date': '2026-08-04 23:30',
+      'tags': ['日常'],
       'content': '今天老婆去剪头发了，短发，她短发挺好看的，但是不是我想象中的那样整齐的，而是另外一种<br/>第二张之前的短发<p class="vh-img-flex"><img src="/assets/images/talking/4befec3764997dc20fce4d60dcab10.JPG"><img src="/assets/images/talking/IMG_1066.JPG"></p>'
     },
     {
-      'date':'2026-08-04 23:12',
-      'tags':['日常', '打麻将'],
+      'date': '2026-08-04 23:12',
+      'tags': ['日常', '打麻将'],
       'content': '我在上饶红星美凯龙开了一家自助麻将馆.不为赚钱，交个朋友'
     },
     {
-      'date':'2026-07-30 15:05:00',
-      'tags':['日常'],
+      'date': '2026-07-30 15:05:00',
+      'tags': ['日常'],
       'content': '今天突然下暴雨，家里的被子没收难受了，然后家里阳台没做好，暴雨直接从阳台流进客厅中'
     },
     {
       'date': '2026-07-26 23:05:00',
-      'tags':['日常', '遥远的救世主'],
+      'tags': ['日常', '遥远的救世主'],
       'content': '股市它实质不产生价值，它只是把多数人口袋里的钱转移到少部分人口袋里。'
     },
     {
       'date': '2026-07-26 22:58:00',
-      'tags':['日常', '遥远的救世主'],
+      'tags': ['日常', '遥远的救世主'],
       'content': '自嘲 -丁元英<br/> 本是后山人，偶做前堂客。<br/>醉舞经阁半卷书，坐井说天阔。<br/>大志戏功名，海斗量福祸。<br/>论到囊中羞涩时，怒指乾坤错'
     },
     {
-      'date':'2026-07-24 22:58:00',
-      'tags':['日常', '废话', '柏拉图'],
+      'date': '2026-07-24 22:58:00',
+      'tags': ['日常', '废话', '柏拉图'],
       'content': '柏拉图说过：一人做事一人当，小叮做事小叮当，当当做事当当当'
     },
     {
-      'date':'2026-07-24 00:00:00',
-      'tags':['日常','图片'],
+      'date': '2026-07-24 00:00:00',
+      'tags': ['日常', '图片'],
       'content': '<p class="vh-img-flex"><img src="/assets/images/talking/people/IMG_0059.JPG"><img src="/assets/images/talking/people/IMG_0062.JPG"></p>'
     },
     {
-      'date':'2026-07-23 14:00:00',
-      'tags':['日常','图片','旅行'],
+      'date': '2026-07-23 14:00:00',
+      'tags': ['日常', '图片', '旅行'],
       'content': '2020武功山之行：徒步登山，穿越草甸，云海翻涌如仙境，星空璀璨似银河。一步一景，皆是难忘的回忆。<p class="vh-img-flex"><img src="/assets/images/talking/wugushan/AAA_3065.JPG"><img src="/assets/images/talking/wugushan/AAA_3106.JPG"><img src="/assets/images/talking/wugushan/AAA_3294.jpg"><img src="/assets/images/talking/wugushan/IMG_6105.JPG"><img src="/assets/images/talking/wugushan/IMG_6128.JPG"><img src="/assets/images/talking/wugushan/IMG_6177.jpg"><img src="/assets/images/talking/wugushan/IMG_6240.JPG"><img src="/assets/images/talking/wugushan/IMG_6242.JPG"><img src="/assets/images/talking/wugushan/IMG_6244.JPG"><img src="/assets/images/talking/wugushan/IMG_6245.JPG"><img src="/assets/images/talking/wugushan/IMG_6246.JPG"><img src="/assets/images/talking/wugushan/IMG_6247.JPG"><img src="/assets/images/talking/wugushan/IMG_6249.JPG"><img src="/assets/images/talking/wugushan/IMG_6250.JPG"><img src="/assets/images/talking/wugushan/IMG_8848.JPG"><img src="/assets/images/talking/wugushan/IMG_8850.JPG"><img src="/assets/images/talking/wugushan/sajdhkj.jpg"></p>'
-     },
+    },
     {
-      'date':'2026-07-23 11:31:06',
-      'tags':['日常'],
+      'date': '2026-07-23 11:31:06',
+      'tags': ['日常'],
       'content': '西西弗书店<p class="vh-img-flex"><img src="/assets/images/talking/people/xixifushudian.jpg"></p>'
     },
     {
-      'date':'2026-07-21 23:31:06',
-      'tags':['日常', '图片'],
+      'date': '2026-07-21 23:31:06',
+      'tags': ['日常', '图片'],
       'content': '<p class="vh-img-flex"><img src="/assets/images/talking/people/IMG_2541.JPG"><img src="/assets/images/talking/people/IMG_2540.JPG"><img src="/assets/images/talking/people/IMG_2539.JPG"></p>'
     },
     {
-      'date':'2026-07-21 23:21:06',
-      'tags':['日常'],
+      'date': '2026-07-21 23:21:06',
+      'tags': ['日常'],
       'content': '今天是宝贝登录地球online的140天，我发现今天她会往前移动了，不是爬，通过小脚往前蹭每次前进一点点，真是一个神奇的现象。'
     },
     {
-      'date':'2026-07-21 23:11:06',
-      'tags':['日常'],
+      'date': '2026-07-21 23:11:06',
+      'tags': ['日常'],
       'content': '今天终于把9年前用表弟身份证办理的手机卡过户回自己名下了。这期间拉着表弟跑了好几趟营业厅，打了无数次电话，时间线拉得很长。<br>2017年刚出社会，想办一张自己的手机卡，让表弟陪我去镇上营业厅。结果我没带身份证，老板说用表弟的也行，当时图方便就没回去拿。<br>多年后子弹正中眉心——很多需要本人身份证办理的业务都因为这张卡受阻。今天终于通过中国移动APP线上过户完成了这件事。<br>得出的经验：很多事情千万不要贪图一时方便，不然后面的麻烦会成倍反噬自己。'
     },
     {
-      'date':'2026-07-21 22:41:06',
-      'tags':['日常', '旅行', '图片'],
-      'content':'香港之旅，穿梭在霓虹闪烁的街头，感受东西方文化交融的独特魅力。维多利亚港的海风、庙街的烟火气、太平山顶的夜景...每一帧都是难忘的记忆。<p class="vh-img-flex"><img src="/assets/images/talking/hangkang/IMG_2491.JPG"><img src="/assets/images/talking/hangkang/IMG_2469.JPG"><img src="/assets/images/talking/hangkang/IMG_2405.JPG"><img src="/assets/images/talking/hangkang/IMG_2448.JPG"><img src="/assets/images/talking/hangkang/IMG_2439.JPG"><img src="/assets/images/talking/hangkang/IMG_2407.JPG"><img src="/assets/images/talking/hangkang/IMG_2426.JPG"><img src="/assets/images/talking/hangkang/IMG_2414.JPG"><img src="/assets/images/talking/hangkang/IMG_2412.JPG"><img src="/assets/images/talking/hangkang/IMG_2402.JPG"><img src="/assets/images/talking/hangkang/IMG_2409.JPG"><img src="/assets/images/talking/hangkang/IMG_2394.JPG"><img src="/assets/images/talking/hangkang/IMG_2380.JPG"><img src="/assets/images/talking/hangkang/IMG_2397.JPG"><img src="/assets/images/talking/hangkang/IMG_2391.JPG"></p>'
+      'date': '2026-07-21 22:41:06',
+      'tags': ['日常', '旅行', '图片'],
+      'content': '香港之旅，穿梭在霓虹闪烁的街头，感受东西方文化交融的独特魅力。维多利亚港的海风、庙街的烟火气、太平山顶的夜景...每一帧都是难忘的记忆。<p class="vh-img-flex"><img src="/assets/images/talking/hangkang/IMG_2491.JPG"><img src="/assets/images/talking/hangkang/IMG_2469.JPG"><img src="/assets/images/talking/hangkang/IMG_2405.JPG"><img src="/assets/images/talking/hangkang/IMG_2448.JPG"><img src="/assets/images/talking/hangkang/IMG_2439.JPG"><img src="/assets/images/talking/hangkang/IMG_2407.JPG"><img src="/assets/images/talking/hangkang/IMG_2426.JPG"><img src="/assets/images/talking/hangkang/IMG_2414.JPG"><img src="/assets/images/talking/hangkang/IMG_2412.JPG"><img src="/assets/images/talking/hangkang/IMG_2402.JPG"><img src="/assets/images/talking/hangkang/IMG_2409.JPG"><img src="/assets/images/talking/hangkang/IMG_2394.JPG"><img src="/assets/images/talking/hangkang/IMG_2380.JPG"><img src="/assets/images/talking/hangkang/IMG_2397.JPG"><img src="/assets/images/talking/hangkang/IMG_2391.JPG"></p>'
     },
     {
-      'date':'2026-07-21 22:31:06',
-      'tags':['语录'],
-      'content':'人生在世，无非是让别人笑笑，偶尔，也笑笑别人。<br>——郭德纲'
+      'date': '2026-07-21 22:31:06',
+      'tags': ['语录'],
+      'content': '人生在世，无非是让别人笑笑，偶尔，也笑笑别人。<br>——郭德纲'
     },
     {
-      'date':'2026-07-21 22:21:06',
-      'tags':['语录'],
-      'content':'自由固不是钱所能买到的，但能够为钱所卖掉。<br>——《娜拉走后怎样》鲁迅'
+      'date': '2026-07-21 22:21:06',
+      'tags': ['语录'],
+      'content': '自由固不是钱所能买到的，但能够为钱所卖掉。<br>——《娜拉走后怎样》鲁迅'
     },
     {
-      'date':'2026-07-21 22:11:06',
-      'tags':['语录'],
-      'content':'楼下一个男人病得要死， 那间隔壁的一家唱着留声机， 对面是弄孩子， 楼上有两人狂笑， 还有打牌声， 河中的船上有女人哭着她死去的母亲， 人类的悲欢并不相通， 我只觉得他们吵闹。<br>——《而已集•小杂感》鲁迅'
+      'date': '2026-07-21 22:11:06',
+      'tags': ['语录'],
+      'content': '楼下一个男人病得要死， 那间隔壁的一家唱着留声机， 对面是弄孩子， 楼上有两人狂笑， 还有打牌声， 河中的船上有女人哭着她死去的母亲， 人类的悲欢并不相通， 我只觉得他们吵闹。<br>——《而已集•小杂感》鲁迅'
     },
     {
-      'date':'2026-07-19 20:41:06',
-      'tags':['日常', '图片'],
-      'content':'最近拍的一些照片，记录生活中的点点滴滴。<p class="vh-img-flex"><img src="/assets/images/talking/people/IMG_2660.JPG"><img src="/assets/images/talking/people/IMG_2770.JPG"><img src="/assets/images/talking/people/IMG_2538.JPG"><img src="/assets/images/talking/people/IMG_2571.JPG"><img src="/assets/images/talking/people/IMG_2537.JPG"><img src="/assets/images/talking/people/IMG_2766.JPG"><img src="/assets/images/talking/people/IMG_2771.JPG"><img src="/assets/images/talking/people/IMG_2667.JPG"><img src="/assets/images/talking/people/IMG_2765.JPG"><img src="/assets/images/talking/people/IMG_2590.JPG"></p>'
+      'date': '2026-07-19 20:41:06',
+      'tags': ['日常', '图片'],
+      'content': '最近拍的一些照片，记录生活中的点点滴滴。<p class="vh-img-flex"><img src="/assets/images/talking/people/IMG_2660.JPG"><img src="/assets/images/talking/people/IMG_2770.JPG"><img src="/assets/images/talking/people/IMG_2538.JPG"><img src="/assets/images/talking/people/IMG_2571.JPG"><img src="/assets/images/talking/people/IMG_2537.JPG"><img src="/assets/images/talking/people/IMG_2766.JPG"><img src="/assets/images/talking/people/IMG_2771.JPG"><img src="/assets/images/talking/people/IMG_2667.JPG"><img src="/assets/images/talking/people/IMG_2765.JPG"><img src="/assets/images/talking/people/IMG_2590.JPG"></p>'
     },
     {
       date: '2026-07-19 18:54:06',
@@ -156,7 +161,7 @@ export default {
     },
     {
       date: '2026-07-17 14:14:06',
-      tags: ['日常','图片', '壁纸'],
+      tags: ['日常', '图片', '壁纸'],
       content: '这些壁纸好像是一个linux的壁纸，挺好看的<p class="vh-img-flex"><img src="/assets/images/talking/sham.png"><img src="/assets/images/talking/hai.jpg"><img src="/assets/images/talking/shu.png"><img src="/assets/images/talking/lu.jpg"><img src="/assets/images/talking/shan.jpg"><img src="/assets/images/talking/hua.jpg"></p>'
     },
     {
